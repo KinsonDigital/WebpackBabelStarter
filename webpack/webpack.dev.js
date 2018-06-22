27d7f651290c171a76055ce8c89e290b246e3a73
Calvin Wilkinson <kinsondigital@gmail.com>
2018-06-22 19:50:55 -0400

    Setup debugging for chrome

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,7 +3,7 @@ const common = require("./webpack.common");
 
 module.exports = merge(common, {
 	mode: "development",
-	devtool: "eval",
+	devtool: "inline-source-map",
 	devServer: {
 		//Used to include html files in the output to detect changes during dev server runtime
 		watchContentBase: true,
