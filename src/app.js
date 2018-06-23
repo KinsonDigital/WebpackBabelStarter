9f3d0a68b98fb93e4144bc6c29b8beeb8036f414
Calvin Wilkinson <kinsondigital@gmail.com>
2018-06-22 20:37:31 -0400

    Cleanup project

--- src/app.js
+++ src/app.js
@@ -1,25 +1,3 @@
-import { person, sayHello } from "./lib";
+import { logger } from "./logger";
 
-console.log(person.name);
-
-sayHello("Kinson");
-
-var request = new Request("https://jsonplaceholder.typicode.com/posts", {
-	method: "GET"
-});
-
-async function getPosts() {
-	/*fetch is a new built in browser function for requesting data from servers.
-		this is an ajax replacement.  Using "async await" requires that you
-		install a polyfill called "babel-polyfill" and "babel-preset-stage-0" npm packages
-	*/
-	const response = await fetch(request);
-
-	const data = await response.json();
-
-	return data;
-}
-
-
-getPosts()
-	.then(posts => console.log(posts));
+logger("Hello World");
--- src/lib.js
+++
@@ -1,9 +0,0 @@
-export const person = {
-	name: "John Doe",
-	location: "Grand Rapids, MI",
-	age: 37
-}
-
-export function sayHello(name) {
-	console.log(`Hello ${name}`);
-}
